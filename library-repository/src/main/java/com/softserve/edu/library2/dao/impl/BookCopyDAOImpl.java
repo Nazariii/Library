0d831083addc2cfa9c4703ef4a20fcdf1488eaaa
volodymyrignatiev <volodymyrignatiev94@gmail.com>
2015-08-21 11:58:36 +0300

    dao22

--- library-repository/src/main/java/com/softserve/edu/library2/dao/impl/BookCopyDAOImpl.java
+++ library-repository/src/main/java/com/softserve/edu/library2/dao/impl/BookCopyDAOImpl.java
@@ -3,10 +3,105 @@ package com.softserve.edu.library2.dao.impl;
 import com.softserve.edu.library2.dao.AbstractDAO;
 import com.softserve.edu.library2.dao.BookCopyDAO;
 import com.softserve.edu.library2.dao.entities.BookCopy;
+import com.softserve.edu.library2.dao.util.HibernateUtil;
+import org.hibernate.Session;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by Dmytro on 8/20/2015.
  */
 public class BookCopyDAOImpl extends AbstractDAO<BookCopy, Integer> implements BookCopyDAO{
+    private Class book;
+    private Long id;
 
+    public void updateBook(Long book_id, BookCopy book) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(book);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+
+    public Collection getAllBooks() throws SQLException {
+        Session session = null;
+        List books = new ArrayList<BookCopy>();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            books = session.createCriteria(BookCopy.class).list();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return books;
+    }
+
+    public void deleteBook(BookCopy book) throws SQLException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(book);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public void save(BookCopy entity) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(entity);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public void merge(BookCopy entity) {
+
+    }
+
+    @Override
+    public void delete(BookCopy entity) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(entity);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
 }
