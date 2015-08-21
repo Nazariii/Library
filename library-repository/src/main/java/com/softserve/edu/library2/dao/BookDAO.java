d7a6d3182f7e06323279e37c9543ac5b17edb9e7
Taras Panychok <taraspanychok@mail.ua>
2015-08-21 14:31:51 +0300

    Add methods to BookDAO. Test them.

--- library-repository/src/main/java/com/softserve/edu/library2/dao/BookDAO.java
+++ library-repository/src/main/java/com/softserve/edu/library2/dao/BookDAO.java
@@ -3,12 +3,17 @@
  */
 package com.softserve.edu.library2.dao;
 
+import java.sql.Date;
+import java.util.List;
+
 import com.softserve.edu.library2.dao.entities.Book;
 
 /**
- * @author Назік
+ * @author Taras
  *
  */
 public interface BookDAO extends GenericDAO<Book, Integer>{
-
+	Book getBookByName(String name);
+	Book getBookByISBN(long isbn);
+	//List<Book> getBooksByYear(Date year);
 }
--- library-repository/src/main/java/com/softserve/edu/library2/dao/impl/BookDAOImpl.java
+++ library-repository/src/main/java/com/softserve/edu/library2/dao/impl/BookDAOImpl.java
@@ -2,107 +2,86 @@ package com.softserve.edu.library2.dao.impl;
 
 import com.softserve.edu.library2.dao.AbstractDAO;
 import com.softserve.edu.library2.dao.BookDAO;
+import com.softserve.edu.library2.dao.entities.Author;
 import com.softserve.edu.library2.dao.entities.Book;
 import com.softserve.edu.library2.dao.util.HibernateUtil;
+
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Query;
 import org.hibernate.Session;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.sql.Date;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * @author Vova
+ * @author Taras
  *
  */
 public class BookDAOImpl extends AbstractDAO<Book, Integer> implements BookDAO {
-        private Class book;
-        private Long id;
 
-        public void updateBook(Long book_id, Book book) throws SQLException {
-            Session session = null;
-            try {
-                session = HibernateUtil.getSessionFactory().openSession();
-                session.beginTransaction();
-                session.update(book);
-                session.getTransaction().commit();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            } finally {
-                if (session != null && session.isOpen()) {
-                    session.close();
-                }
-            }
-        }
+	private static Logger logger = org.apache.logging.log4j.LogManager.getLogger();
+	
+	/**
+	 * Method finds book by name
+	 * 
+	 * @param name
+	 *            - name of book
+	 * @return {@link Book}
+	 */
+	@Override
+	public Book getBookByName(String name) {
+		String sql = "FROM Book WHERE name = :name";
+		Query query = HibernateUtil.getSession().createQuery(sql).setString("name", name);
+		Book book = null;
+		try {
+			book = findOne(query);
+		} catch (Exception e) {
+			logger.error("Error", e);
+		}
+		return book;
+	}
+	
+	/**
+	 * Method finds book by isbn
+	 * 
+	 * @param isbn
+	 *            - isbn of book
+	 * @return {@link Book}
+	 */
+	@Override
+	public Book getBookByISBN(long isbn) {
+		String sql = "FROM Book WHERE isbn = :isbn";
+		Query query = HibernateUtil.getSession().createQuery(sql).setLong("isbn", isbn);
+		Book book = null;
+		try {
+			book = findOne(query);
+		} catch (Exception e) {
+			logger.error("Error", e);
+		}
+		return book;
+	}
 
+	
+	/**
+	 * Method finds all books published in {@param year}
+	 * 
+	 * @param year
+	 *            - year of publishing
+	 * @return {@link List<Book>}
+	 *
+	@Override
+	public List<Book> getBooksByYear(Date year) {
+		String sql = "FROM Book WHERE year = :year";
+		Query query = HibernateUtil.getSession().createQuery(sql).setInteger("year", year.getYear());
+		List<Book> books = Collections.emptyList();
+		try {
+			books = findMany(query);
+		} catch (Exception e) {
+			logger.error("Error", e);
+		}
+		return books;
+	}
 
-        public Collection getAllBooks() throws SQLException {
-            Session session = null;
-            List books = new ArrayList<Book>();
-            try {
-                session = HibernateUtil.getSessionFactory().openSession();
-                books = session.createCriteria(Book.class).list();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            } finally {
-                if (session != null && session.isOpen()) {
-                    session.close();
-                }
-            }
-            return books;
-        }
-
-        public void deleteBook(Book book) throws SQLException {
-            Session session = null;
-            try {
-                session = HibernateUtil.getSessionFactory().openSession();
-                session.beginTransaction();
-                session.delete(book);
-                session.getTransaction().commit();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            } finally {
-                if (session != null && session.isOpen()) {
-                    session.close();
-                }
-            }
-        }
-
-        @Override
-        public void save(Book entity) {
-            Session session = null;
-            try {
-                session = HibernateUtil.getSessionFactory().openSession();
-                session.beginTransaction();
-                session.save(entity);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            } finally {
-                if (session != null && session.isOpen()) {
-                    session.close();
-                }
-            }
-        }
-
-        @Override
-        public void merge(Book entity) {
-
-        }
-
-        @Override
-        public void delete(Book entity) {
-            Session session = null;
-            try {
-                session = HibernateUtil.getSessionFactory().openSession();
-                session.beginTransaction();
-                session.delete(book);
-                session.getTransaction().commit();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            } finally {
-                if (session != null && session.isOpen()) {
-                    session.close();
-                }
-            }
-        }
+	*/
 }
