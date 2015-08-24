06beb693b92bf2ff57b80e9dcfdc040e96dcc8a5
nazariii <nazarii.ivashkiv>
2015-08-24 14:52:15 +0300

    - have added Spring support - have refactored hibernate properties to java-style spring properties

---
+++ library-repository/src/main/java/com/softserve/edu/library2/config/AppConfig.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.softserve.edu.library2.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Java-style configuration of Spring
+ * 
+ * @author Назік.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.softserve.edu.library2")
+public class AppConfig {
+
+}
