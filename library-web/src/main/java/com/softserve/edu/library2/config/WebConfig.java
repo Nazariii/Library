525bde0bf821f2f105fcd76d258dddbb7ff4fd33
Taras Panychok <taraspanychok@mail.ua>
2015-08-25 21:44:40 +0300

    Failed attempt to make it work.

--- library-web/src/main/java/com/softserve/edu/library2/config/WebConfig.java
+++ library-web/src/main/java/com/softserve/edu/library2/config/WebConfig.java
@@ -1,30 +1,39 @@
+/**
+ * 
+ */
 package com.softserve.edu.library2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 /**
  * @author Taras
  *
  */
 @Configuration
-// Marks this class as configuration
-// Specifies which package to scan
-@ComponentScan("com.softserve.edu.library")
-// Enables Spring's annotations
 @EnableWebMvc
-public class WebConfig {
+@ComponentScan("com.softserve.edu.library2")
+public class WebConfig extends WebMvcConfigurerAdapter {
 
-	@Bean
-	public UrlBasedViewResolver setupViewResolver() {
-		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-		resolver.setPrefix("/WEB-INF/pages/");
-		resolver.setSuffix(".jsp");
-		resolver.setViewClass(JstlView.class);
-		return resolver;
-	}
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/WEB-INF/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }
+
 }
---
+++ library-web/src/main/java/com/softserve/edu/library2/controller/IndexController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.softserve.edu.library2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Taras
+ *
+ */
+@Controller
+@RequestMapping("/welcome")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+    	
+        model.addAttribute("message", "Spring 3 MVC - Hello World");
+        return "hello";
+
+    }
+    
+}
