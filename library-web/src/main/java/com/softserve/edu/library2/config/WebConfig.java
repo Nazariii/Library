b25c59ec1e50ab1fcbfe35c1abff08c81dea1a12
Taras Panychok <taraspanychok@mail.ua>
2015-08-25 18:44:52 +0300

    Add controller and web config. Dunno whether it works but need backup copy somewhere.

---
+++ library-web/src/main/java/com/softserve/edu/library2/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.softserve.edu.library2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+/**
+ * @author Taras
+ *
+ */
+@Configuration
+// Marks this class as configuration
+// Specifies which package to scan
+@ComponentScan("com.softserve.edu.library")
+// Enables Spring's annotations
+@EnableWebMvc
+public class WebConfig {
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+}
---
+++ library-web/src/main/java/com/softserve/edu/library2/config/WebInitializer.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.softserve.edu.library2.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/**
+ * @author Taras
+ *
+ */
+public class WebInitializer implements WebApplicationInitializer {
+	 
+public void onStartup(ServletContext servletContext) throws ServletException {
+ 
+ AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+ ctx.register(WebConfig.class);
+ 
+ ctx.setServletContext(servletContext);
+ 
+ Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+ servlet.addMapping("/");
+ servlet.setLoadOnStartup(1);
+ 
+ }
+ 
+}
